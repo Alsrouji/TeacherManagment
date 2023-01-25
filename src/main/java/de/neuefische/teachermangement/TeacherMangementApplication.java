e15eb1ae356b8c230a3e33307c189e0c07359c1a
Alsrouji <mohsy1973@gmail.com>
2023-01-25 13:29:54 +0100

    init: teach

---
+++ src/main/java/de/neuefische/teachermangement/TeacherMangementApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.teachermangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherMangementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeacherMangementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/teachermangement/TeacherMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.teachermangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeacherMangementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
